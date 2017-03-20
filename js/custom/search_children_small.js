64625ec299d2de88abc432a0dc2ebdb1498beecc
CoderJay <396736694@qq.com>
2017-03-20 12:34:57 +0800

    Site updated: 2017-03-20 12:34:57

--- js/custom/search_children_small.js
+++
@@ -1,5 +0,0 @@
-var _base = 'http://qzone.qq.com/gy/404/';
-document.write('<script type="text/javascript" src="' + _base + 'data.js" charset="utf-8"></script>');
-document.write('<script type="text/javascript" src="/js/custom/page.js"  charset="utf-8"></script>');
-// document.write('<script type="text/javascript" src="'+ _base + 'page.js" charset="utf-8"></script>');
-/*  |xGv00|bb048611f4d0bb34934fd40353d94edf */
