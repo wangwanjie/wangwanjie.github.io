40824f77a6760d3336fa08229e2bc8d6e5ba74b0
VanJay <wangwanjie1993@gmail.com>
2018-10-02 16:27:53 +0800

    Site updated: 2018-10-02 16:27:53

--- js/src/motion.js
+++ js/src/motion.js
@@ -54,8 +54,8 @@ $(document).ready(function() {
   var sidebarToggleLine1st = new SidebarToggleLine({
     el    : '.sidebar-toggle-line-first',
     status: {
-      arrow: {width: '50%', rotateZ: '-45deg', top: '2px'},
-      close: {width: '100%', rotateZ: '-45deg', top: '5px'}
+      arrow: { width: '50%', rotateZ: '45deg', top: '2px', left: '5px' },
+      close: { width: '100%', rotateZ: '45deg', top: '5px', left: 0 }
     }
   });
   var sidebarToggleLine2nd = new SidebarToggleLine({
@@ -68,8 +68,8 @@ $(document).ready(function() {
   var sidebarToggleLine3rd = new SidebarToggleLine({
     el    : '.sidebar-toggle-line-last',
     status: {
-      arrow: {width: '50%', rotateZ: '45deg', top: '-2px'},
-      close: {width: '100%', rotateZ: '45deg', top: '-5px'}
+      arrow: { width: '50%', rotateZ: '-45deg', top: '-2px', left: '5px' },
+      close: { width: '100%', rotateZ: '-45deg', top: '-5px', left: 0 }
     }
   });
 
@@ -98,7 +98,7 @@ $(document).ready(function() {
       $(document)
         .on('sidebar.isShowing', function() {
           NexT.utils.isDesktop() && $('body').velocity('stop').velocity(
-            {paddingRight: SIDEBAR_WIDTH},
+            { paddingLeft: SIDEBAR_WIDTH},
             SIDEBAR_DISPLAY_DURATION
           );
         })
@@ -164,7 +164,7 @@ $(document).ready(function() {
       this.sidebarEl.trigger('sidebar.isShowing');
     },
     hideSidebar: function() {
-      NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingRight: 0});
+      NexT.utils.isDesktop() && $('body').velocity('stop').velocity({paddingLeft: 0});
       this.sidebarEl.find('.motion-element').velocity('stop').css('display', 'none');
       this.sidebarEl.velocity('stop').velocity({width: 0}, {display: 'none'});
 
